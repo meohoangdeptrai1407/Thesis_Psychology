ef58684c91cfa2e119b56b587bbbc5231e93e025
Nghia Le <66348769+meohoangdeptrai1407@users.noreply.github.com>
2023-09-13 15:58:19 +0700

    Create GetDataSetKaggle.py

---
+++ Voice/Voice Emotion Recognition/DataSet/GetDataSetKaggle.py
@@ -0,0 +1,22 @@
+import os
+import kaggle
+
+# Set Kaggle API credentials
+os.environ['KAGGLE_USERNAME'] = '' # Your username in kaggle
+os.environ['KAGGLE_KEY'] = '' # Your kaggle key, inside the settings
+
+# Define dataset details
+dataset_name = r'ejlok1/toronto-emotional-speech-set-tess' # get Dataset link, replace with your dataset
+output_dir = r'D:\Thesis work\Emotional Voice'  # Replace with your desired output directory
+
+# Create the output directory if it doesn't exist
+os.makedirs(output_dir, exist_ok=True)
+
+# Print a message indicating the dataset is ready to download
+print("The TESS dataset is ready to download.")
+
+# Download and extract the dataset
+kaggle.api.dataset_download_files(dataset_name, path=output_dir, unzip=True)
+
+# Print a message when the download is complete
+print("Dataset downloaded and extracted successfully!")
